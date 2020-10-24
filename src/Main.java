3482a545c9ea68590143e3e14f7c60a433599e7d
Chereshnev Nikita <chereshnev.n.s@edu.mirea.ru>
2020-10-24 14:39:30 +0300

    AplusB

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Nikita");
+import java.util.*;
 
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int a, b;
+        a = in.nextInt();
+        b = in.nextInt();
+        int c = a + b;
+        System.out.println(c);
     }
 }
+
