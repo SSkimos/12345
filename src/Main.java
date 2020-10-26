d45d9867663dd8d33fe4ee5f4ed167cb7f306c36
Chereshnev Nikita <chereshnev.n.s@edu.mirea.ru>
2020-10-26 13:30:17 +0300

    Purchase value

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,16 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);//ввод
-        int a, h, m;
-        String mr, sr;
+        int a, b, n;
         a = in.nextInt();
-        h = a / 3600;
-        a -= h*3600;
-        m = a / 60;
-        a -= m*60;
-        h = h % 24;
-        if (m < 10) {
-            mr = "0" + String.valueOf(m);
-        } else {
-            mr = String.valueOf(m);
-        }
-        if (a < 10) {
-            sr = "0" + String.valueOf(a);
-        } else {
-            sr = String.valueOf(a);
-        }
-        System.out.println(h + ":" + mr + ":" + sr); //вывод
+        b = in.nextInt();
+        n = in.nextInt();
+        a *= n;
+        b *= n;
+        a += b / 100;
+        b %= 100;
+        System.out.println(a + " " + b); //вывод
 
     }
 
 }
-
