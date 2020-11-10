2db5f91b83a48751afee4ad226596645aad4e36b
Chereshnev Nikita <chereshnev.n.s@edu.mirea.ru>
2020-11-10 12:10:01 +0300

    Simplicity check

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);//ввод
-        int a, b, n;
-        a = in.nextInt();
-        b = in.nextInt();
-        n = in.nextInt();
-        a *= n;
-        b *= n;
-        a += b / 100;
-        b %= 100;
-        System.out.println(a + " " + b); //вывод
+        Scanner in = new Scanner(System.in);//ввод
 
+        int number = in.nextInt();
+        int i = 2;
+        boolean bool = false;
+
+        while (i <= number / 2) {
+            if (number % i == 0) {
+                bool = true;
+                break;
+            }
+            i++;
+        }
+        if (!bool) {
+            System.out.println("prime");
+            return;
+        }
+        System.out.println("composite");
     }
-
 }
