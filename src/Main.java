f66f91ab9c6e5e23d55e32b14db3a6049c9d001e
Chereshnev Nikita <chereshnev.n.s@edu.mirea.ru>
2020-11-13 00:40:58 +0300

    spiral

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,49 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);//ввод
-        double a = in.nextDouble();
-        double b = in.nextDouble();
 
-        if (a == 0){
-            if (b > 0)
-                System.out.println("any x");
-            else if (b <= 0)
-                System.out.println("no such x");
+        int n = in.nextInt();
+
+        int[][] a = new int[2*n+1][2*n+1];
+
+        a[n][n] = 0;
+
+        int x = n;
+        int y = n;
+
+        int i = 1;
+        int k = 1;
+
+        while (true) {
+            for (int j = 0; j < k ; j++) {
+                a[x][--y]=i++;
+                if (i == (2*n+1)*(2*n+1)) break;
+            }
+            if (i == (2*n+1)*(2*n+1)) break;
+            for (int j = 0; j < k ; j++) {
+                a[--x][y] = i++;
+            }
+            k++;
+            for (int j = 0; j < k; j++) {
+                a[x][++y] = i++;
+                if (i == (2*n+1)*(2*n+1)) break;
+            }
+            if (i == (2*n+1)*(2*n+1)) break;
+            for (int j = 0; j < k; j++) {
+                a[++x][y]=i++;
+            }
+            k++;
         }
-        else if (a < 0 && b < 0)
-            System.out.println("no such x");
-        else if (b > 0 && a > 0)
-            System.out.println("any x");
-        else {
-            String buffer1 = String.format("%.1f", b/a);
-            String buffer2 = String.format("%.1f", -1*b/a);
-            if (a > 0)
-                System.out.println("x<" + buffer1 + " or x>" + buffer2);
-            else System.out.println(buffer1 + "<x<" + buffer2);
+
+        for(i = 0; i < 2*n + 1; i++){
+            for(int j = 0; j < 2*n + 1; j++){
+                if(a[j][i] < 10)
+                    System.out.print("  ");
+                if(a[j][i] >= 10 && a[j][i] < 100)
+                    System.out.print(" ");
+                System.out.print(a[j][i]);
+            }
+            System.out.println("");
         }
     }
 }
